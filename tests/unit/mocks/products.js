a04388ff28e67a3ea1fca147df321ef5f3394b8b
Bruno Pessoa <brunofernpessoa@gmail.com>
2022-10-18 20:15:46 -0300

    Padronização de código

--- tests/unit/mocks/products.js
+++ tests/unit/mocks/products.js
@@ -1,22 +1,22 @@
 const allProductsMock = [
   {
-    "id": 1,
-    "name": "Martelo de Thor"
+    id: 1,
+    name: 'Martelo de Thor'
   },
   {
-    "id": 2,
-    "name": "Traje de encolhimento"
+    id: 2,
+    name: 'Traje de encolhimento'
   }
 ];
 
 const productByIdMock = {
-  "id": 1,
-  "name": "Martelo de Thor"
+  id: 1,
+  name: 'Martelo de Thor'
 };
 
 const insertedProductMock = {
-  "id": 9999,
-  "name": "product x",
+  id: 9999,
+  name: 'product x',
 }
 
 const updateResultMock = [
--- tests/unit/mocks/sales.js
+++ tests/unit/mocks/sales.js
@@ -1,119 +1,119 @@
 const salesMock = [
   {
-    "productId": 1,
-    "quantity": 1
+    productId: 1,
+    quantity: 1
   },
   {
-    "productId": 2,
-    "quantity": 5
+    productId: 2,
+    quantity: 5
   }
 ];
 
 const salesWithoutProductId = [
   {
-    "quantity": 5
+    quantity: 5
   }
 ];
 
 const salesWithoutQuantity = [
   {
-      "productId": 1
+      productId: 1
   }
 ];
 
 const salesWrongQuantity = [
   {
-    "productId": 1,
-    "quantity": 0
+    productId: 1,
+    quantity: 0
   }
 ];
 
 const saleswithWrongProductId = [
   {
-    "productId": 9999,
-    "quantity": 1
+    productId: 9999,
+    quantity: 1
   }
 ];
 
 const salesSuccessResposeMock = {
-  "id": 3,
-  "itemsSold": [
+  id: 3,
+  itemsSold: [
     {
-      "productId": 1,
-      "quantity": 1
+      productId: 1,
+      quantity: 1
     },
     {
-      "productId": 2,
-      "quantity": 5
+      productId: 2,
+      quantity: 5
     }
   ]
 };
 
 const allSalesMock = [
   {
-    "saleId": 1,
-    "date": "2022-10-14T15:41:59.000Z",
-    "productId": 1,
-    "quantity": 5
+    saleId: 1,
+    date: '2022-10-14T15:41:59.000Z',
+    productId: 1,
+    quantity: 5
   },
   {
-    "saleId": 1,
-    "date": "2022-10-14T15:41:59.000Z",
-    "productId": 2,
-    "quantity": 10
+    saleId: 1,
+    date: '2022-10-14T15:41:59.000Z',
+    productId: 2,
+    quantity: 10
   },
   {
-    "saleId": 2,
-    "date": "2022-10-14T15:41:59.000Z",
-    "productId": 3,
-    "quantity": 15
+    saleId: 2,
+    date: '2022-10-14T15:41:59.000Z',
+    productId: 3,
+    quantity: 15
   }
 ];
 
 const allSalesDbMock = [
   {
-    "sale_id": 1,
-    "date": "2022-10-14T15:41:59.000Z",
-    "product_id": 1,
-    "quantity": 5
+    sale_id: 1,
+    date: '2022-10-14T15:41:59.000Z',
+    product_id: 1,
+    quantity: 5
   },
   {
-    "sale_id": 1,
-    "date": "2022-10-14T15:41:59.000Z",
-    "product_id": 2,
-    "quantity": 10
+    sale_id: 1,
+    date: '2022-10-14T15:41:59.000Z',
+    product_id: 2,
+    quantity: 10
   },
   {
-    "sale_id": 2,
-    "date": "2022-10-14T15:41:59.000Z",
-    "product_id": 3,
-    "quantity": 15
+    sale_id: 2,
+    date: '2022-10-14T15:41:59.000Z',
+    product_id: 3,
+    quantity: 15
   }
 ];
 
 const saleByIdMock = [
   {
-    "date": "2022-10-14T15:41:59.000Z",
-    "productId": 1,
-    "quantity": 5
+    date: '2022-10-14T15:41:59.000Z',
+    productId: 1,
+    quantity: 5
   },
   {
-    "date": "2022-10-14T15:41:59.000Z",
-    "productId": 2,
-    "quantity": 10
+    date: '2022-10-14T15:41:59.000Z',
+    productId: 2,
+    quantity: 10
   },
 ];
 
 const saleByIdDbMock = [
   {
-    "date": "2022-10-14T15:41:59.000Z",
-    "product_id": 1,
-    "quantity": 5
+    date: '2022-10-14T15:41:59.000Z',
+    product_id: 1,
+    quantity: 5
   },
   {
-    "date": "2022-10-14T15:41:59.000Z",
-    "product_id": 2,
-    "quantity": 10
+    date: '2022-10-14T15:41:59.000Z',
+    product_id: 2,
+    quantity: 10
   },
 ];
 
@@ -144,12 +144,12 @@ const updateResultMock = [
 
 const updatedSaleMock = [
   {
-    "productId": 1,
-    "quantity": 10
+    productId: 1,
+    quantity: 10
   },
   {
-    "productId": 2,
-    "quantity": 50
+    productId: 2,
+    quantity: 50
   }
 ];
 

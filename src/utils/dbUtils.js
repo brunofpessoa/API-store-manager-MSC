4dc1c1a7f32780303aa1401e4581fda02ddd93f9
Bruno Pessoa <brunofernpessoa@gmail.com>
2022-10-18 20:50:37 -0300

    Adiciona credenciais padrão caso .env não exista

--- src/utils/dbUtils.js
+++ src/utils/dbUtils.js
@@ -6,9 +6,9 @@ require('dotenv').config();
 const { cwd } = process;
 
 const connect = () => mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
+  host: process.env.MYSQL_HOST || 'localhost',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'password',
   multipleStatements: true,
 });
 
